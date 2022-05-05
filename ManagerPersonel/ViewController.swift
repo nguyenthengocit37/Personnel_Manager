fe3553f31d4013aae66a54d3b6721fa882aae472
Nguyen The Ngoc <nguyenthengocit37@gmail.com>
2022-05-04 20:50:48 -0700

    Initial Commit

---
+++ ManagerPersonel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManagerPersonel
+//
+//  Created by Nguyen The Ngoc on 5/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
