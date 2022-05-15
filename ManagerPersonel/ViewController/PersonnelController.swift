075fe2b20ba78542a542d4909d597e4d5b8be579
Pham Hung Cuong <cuongp948@gmail.com>
2022-05-15 18:26:04 +0700

    Cuong

--- ManagerPersonel/ViewController/PersonnelController.swift
+++ ManagerPersonel/ViewController/PersonnelController.swift
@@ -13,7 +13,7 @@ class PersonnelController: UITableViewController {
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        // self.clearsSelectionOnViewWillAppear = fals
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
