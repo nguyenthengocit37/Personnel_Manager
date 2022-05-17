6087f3a37865962dc96b960bd4ddf55dcf131899
Pham Hung Cuong <cuongp948@gmail.com>
2022-05-17 22:37:34 +0700

    Cuong

--- ManagerPersonel/Controls/DepartmentTableViewCell.swift
+++ ManagerPersonel/Controls/DepartmentTableViewCell.swift
@@ -19,7 +19,7 @@ class DepartmentTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+        // Configure the view for the selected stat
     }
 
 }
